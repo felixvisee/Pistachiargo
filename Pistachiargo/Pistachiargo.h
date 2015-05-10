5cc50357d4f2bada1435748e9c6b0a6d6620478d
Felix Jendrusch <felix@felixjendrusch.is>
2015-05-10 13:00:35 +0200

    Cleanup imports

--- Pistachiargo/Pistachiargo.h
+++ Pistachiargo/Pistachiargo.h
@@ -1,6 +1,6 @@
 //  Copyright (c) 2015 Felix Jendrusch. All rights reserved.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 //! Project version number for Pistachiargo.
 FOUNDATION_EXPORT double PistachiargoVersionNumber;
